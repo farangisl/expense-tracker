ba9f56fdff3bbd6ae163cb507987a03b98fe103b
Farangis <Farangis>
2023-03-02 09:05:35 +0500

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Махмадёрова Фарангис Шухратовна on 02.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
